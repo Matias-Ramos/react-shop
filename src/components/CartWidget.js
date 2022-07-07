734818ae27dd149c174ff509fea5a98e9f0865d5
Matias <mattsk97@gmail.com>
2022-07-06 22:21:45 -0300

    NavBar y CardWidget comenzados

---
+++ src/components/CartWidget.js
@@ -0,0 +1,6 @@
+const CartWidget = () => {
+    return(
+        <img src="../../images/cart_icon.png" alt='Cart icon' width="25"/>
+    );
+}
+export default CartWidget;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,27 @@
 import * as Btsp from 'react-bootstrap';
+import CartWidget from './CartWidget';
 
 const NavBar = () => {
     return(
       <>
+        
         <Btsp.Navbar bg="primary" variant="dark">
             <Btsp.Container>
                 <Btsp.Navbar.Brand href="#home">TiendaMia</Btsp.Navbar.Brand>
                 <Btsp.Nav className="me-auto">
                     <Btsp.Nav.Link href="#features">Productos</Btsp.Nav.Link>
-                    <Btsp.Nav.Link href="#pricing">Carrito</Btsp.Nav.Link>
+                    <Btsp.Nav.Link href="#pricing" className='h-100'>
+                      <div className="h-100">
+                        <CartWidget />
+                      </div>
+                    </Btsp.Nav.Link>
                     <Btsp.Form className="d-flex">
                         <Btsp.FormControl
                         type="search"
                         placeholder="Buscar productos..."
                         className="me-2"
                         aria-label="Search"/>
-                        <Btsp.Button variant="dark"><img src="https://icongr.am/octicons/search.svg?size=25&color=ffffff"/></Btsp.Button>
+                        <Btsp.Button variant="dark"><img src="https://icongr.am/octicons/search.svg?size=25&color=ffffff" alt="Search icon"/></Btsp.Button>
                     </Btsp.Form>
                 </Btsp.Nav>
             </Btsp.Container>
@@ -25,3 +31,4 @@ const NavBar = () => {
   }
 
 export default NavBar;
+
